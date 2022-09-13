edd486c8d356e86b9ee691d562649e4ef7be81da
thụ nguyễn <110140882+nguyenhuuthu@users.noreply.github.com>
2022-09-13 15:01:49 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let arrObj = [];
+// let arrObj = [];
 let objects = {
   id: null,
   title: "",
@@ -10,21 +10,22 @@ function fn(exception, jsonData) {
   arrObj = [];
   const arrTitleResponse = jsonData[1];
   if (Array.isArray(jsonData)) {
+    console.log(jsonData);
     let arrTitle = [];
     for (let index = 0; index < arrTitleResponse.length; index++) {
       arrTitle.push(arrTitleResponse[index]);
+      console.log(arrTitle);
     }
     for (let index = 0; index < arrTitle.length; index++) {
       objects = {
         id: index,
         title: arrTitle[index],
         description: "",
-        image: "",
       };
       arrObj.push(objects);
     }
     arrObj.forEach((element) => {
-      url = `https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=pageprops|pageimages&format=json&titles=${element.title}`;
+      url = `https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=pageprops|pageimages&format=json&titles=${element}`;
       getData(url, getThumbnail);
     });
     addElement();
@@ -87,3 +88,49 @@ function getData(url, fn) {
   xhr.open("GET", url, true);
   xhr.send();
 }
+
+function getData(url, fn) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        fn(undefined, JSON.parse(xhr.responseText));
+      } else {
+        fn(new Error(xhr.statusText), undefined);
+      }
+    }
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+}
+
+// bai 2
+let input = document.getElementById("input");
+let wrapper = document.getElementById("wrapper");
+
+input.addEventListener(`keyup`, function () {
+  console.log(input.value);
+  wrapper.innerHTML = "";
+  getData(
+    `https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&limit=10&format=json&search=${input.value}`,
+    function (err, res) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(res);
+        let sugestion = res[1].filter(function (eee) {
+          return eee.toLowerCase().startsWith(input.value);
+        });
+        sugestion.forEach((element) => {
+          let div = document.createElement("div");
+          div.innerHTML = `<div class="wrapper-1"><img src="./Saved Pictures/anh 1.png" alt="" class="img"><div class="wrapper-2"><h4>${element}</h4><p>Description</p></div></div>`;
+          wrapper.appendChild(div);
+        });
+        if (input.value === ``) {
+          wrapper.innerHTML = ``;
+        }
+        console.log(sugestion);
+      }
+    }
+  );
+});
